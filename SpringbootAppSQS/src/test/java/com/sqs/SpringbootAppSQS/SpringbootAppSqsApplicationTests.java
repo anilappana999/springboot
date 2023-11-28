90abb090fb9091b6b51315b06489dcc9fa97c0d1
anil <anilappana999@gmail.com>
2023-11-28 17:55:52 +0530

    springboot-sqs

---
+++ SpringbootAppSQS/src/test/java/com/sqs/SpringbootAppSQS/SpringbootAppSqsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqs.SpringbootAppSQS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAppSqsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
